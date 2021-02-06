962b7c9bf41745e6f3de33751751a2ab904579ec
ustc-chris <58923631+ustc-chris@users.noreply.github.com>
2021-02-06 11:12:06 +0800

    Update main.cc

--- 代码/生成起火点/main.cc
+++ 代码/生成起火点/main.cc
@@ -1,6 +1,15 @@
 #include<stdio.h>
+#include <iostream>
+using namespace std;
 
 #define PEOPLE 20
+
+void setfire_start();
+void setfire_size();
+void setfire_firemen();
+void setfire_write_file();
+int Possion_sample(double lambda);
+int JieCheng(int i);
 
 int main()
 {
@@ -11,6 +20,11 @@ int main()
 	setfire_size();
 	setfire_firemen();
 	setfire_write_file();
+	for(int i=0;i<10;++1)
+	{
+		cout<<i<<"!="<<JieCheng(i)<<endl;
+		getchar();
+	}
 	return 0;
 }
 
@@ -29,4 +43,33 @@ void setfire_firemen()
 void setfire_write_file()
 {
 	;
+}
+
+int Possion_sample(double lambda)
+// 抽样，得到泊松分布
+{
+	// p是0-1随机数
+	double p=(rand()%1000)/1000.;
+	int k=0;
+	double count=0;
+	for(;count=0;k++)
+	{
+		count += pow(lambda,k)/JieCheng(k)*exp(-lambda);
+		if(count > p)break;
+	}
+	return k;
+}
+
+int JieCheng(int i)
+// 返回i的阶乘
+{
+	int result=1;
+	if(i<0){printf("ERROR in JieCheng();(阶乘计算有误)！\n");getchar();return -1;}
+	if(i==0)return 1;
+	for(;;i--)
+	{
+		result*=i;
+		if(i==1)break;
+	}
+	return result;
 }
