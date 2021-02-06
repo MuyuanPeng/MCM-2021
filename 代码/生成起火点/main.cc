9fc890336dcb29f2b45c7de1b432589e45ccfd05
ustc-chris <58923631+ustc-chris@users.noreply.github.com>
2021-02-06 10:40:25 +0800

    Create main.cc

---
+++ 代码/生成起火点/main.cc
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+#define PEOPLE 20
+
+int main()
+{
+	// 1.在半径为10km的范围内抽样，得到起火点
+	// 2.对于每个起火点，抽样得到起火大小
+	// 3.根据起火大小，抽样得到消防人员的位置
+	setfire_start();
+	setfire_size();
+	setfire_firemen();
+	setfire_write_file();
+	return 0;
+}
+
+void setfire_start()
+{
+	;
+}
+void setfire_size()
+{
+	;
+}
+void setfire_firemen()
+{
+	;
+}
+void setfire_write_file()
+{
+	;
+}
